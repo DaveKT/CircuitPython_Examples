4205822071b4bf15d1dded09927d1b87f85935d6
David Tassara <phi.fibonacci@gmail.com>
2017-06-21 20:09:00 -0700

    Adding an app that demonstrates photoresistor use.

---
+++ max_light_reading.py
@@ -0,0 +1,23 @@
+# Author:   David Kolet-Tassara
+# Date:     June 21, 2017
+# Purpose:  Demonstrates the use of the photoresistor on Adafruit's
+#           Circuit Python. Prints max light reading.
+# Required Packages:
+# No additional packages Required
+
+import board
+import time
+import analogio
+
+light = analogio.AnalogIn(board.LIGHT)
+x = 0
+
+while True:
+    r = light.value
+    if x < r:
+        x = r
+        print(x)
+
+while True:
+    readMaxLight()
+    time.sleep(.1)
